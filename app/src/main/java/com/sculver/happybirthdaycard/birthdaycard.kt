dce9b76d0023b130d7f0fc18e25f385c1358ac02
Nelly <shernellsos@gmail.com>
2022-06-06 21:38:04 -0500

    inital commit

---
+++ app/src/main/java/com/sculver/happybirthdaycard/birthdaycard.kt
@@ -0,0 +1,230 @@
+package com.sculver.happybirthdaycard
+
+fun main() {
+    val age = 24
+    val layers = 5
+    printCakeCandles(age)
+    printCakeTop(age)
+    printCakeBottom(age, layers)
+}
+fun printCakeTop(age: Int) {
+    repeat(age + 2) {
+        print("=")
+    }
+    println() //Print an empty line
+}
+
+fun printCakeCandles(age: Int) {
+      print(" ")
+        repeat(age) {
+            print(",")
+        }
+        println() //Print an empty line
+
+        print(" ") //Print the inset of the candles on the cake
+        repeat(age) {
+            print ("|")
+        }
+        println()
+
+}
+
+fun printCakeBottom (age: Int, layers: Int) {
+    repeat(layers) {
+        repeat(age + 2) {
+            print("@")
+        }
+        println()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
